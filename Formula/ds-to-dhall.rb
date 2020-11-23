5fe69e10d260dc06a011a7e98ff182296fae72ba
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-23 14:16:47 -0500

    Brew formula update for ds-to-dhall version v0.11.0

--- Formula/ds-to-dhall.rb
+++ Formula/ds-to-dhall.rb
@@ -2,17 +2,16 @@
 class DsToDhall < Formula
   desc "CLI to translate deploy-sourcegraph YAML to Dhall"
   homepage "https://github.com/sourcegraph/ds-to-dhall"
-  version "0.10.0"
+  version "0.11.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sourcegraph/ds-to-dhall/releases/download/v0.10.0/ds-to-dhall_0.10.0_Darwin_x86_64.tar.gz"
-    sha256 "3131bc0cfaa104fd8c87d11cc88d2ae99ba66c258140d113b5d8e0d27655d623"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/sourcegraph/ds-to-dhall/releases/download/v0.10.0/ds-to-dhall_0.10.0_Linux_x86_64.tar.gz"
-      sha256 "ed0d22171dc45d615df48bef3a2d55b396c807cca932fd143c344727b549c240"
-    end
+    url "https://github.com/sourcegraph/ds-to-dhall/releases/download/v0.11.0/ds-to-dhall_0.11.0_Darwin_x86_64.tar.gz"
+    sha256 "18cc87906d02b7762c9382b32b11bf51e648bfd2041b195c8a4d4ee926128aac"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/sourcegraph/ds-to-dhall/releases/download/v0.11.0/ds-to-dhall_0.11.0_Linux_x86_64.tar.gz"
+    sha256 "a07b1f0351a77f42deb6ddc45454ce27931627a0dae56e0732c14a2929a36222"
   end
 
   def install
